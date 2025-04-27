ae219f4097251c95e214b6cad2d537171dfb7d90
LiangYaJie00 <127167747+LiangYaJie00@users.noreply.github.com>
2025-04-27 11:05:39 +0800

    remove tuicallkit-kt/build folder all file out of control

---
+++ app/src/main/java/com/example/chms_android/features/mine/activity/UserEditActivity.kt
@@ -0,0 +1,21 @@
+package com.example.chms_android.features.mine.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.chms_android.R
+
+class UserEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_user_edit)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
